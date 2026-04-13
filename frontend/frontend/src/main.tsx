ff51fed9bfdd8a3803228781f3b5ad646af4cc9c
aal2015 <abhinavlugun@yahoo.com>
2026-04-13 20:57:33 +0530

    Added protected routes

--- frontend/frontend/src/main.tsx
+++ frontend/frontend/src/main.tsx
@@ -1,5 +1,6 @@
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from "react-router";
+import ProtectedRoute from './pages/ProtectedRoute.tsx';
 import Lobby from './pages/Lobby.tsx';
 import './index.css'
 import App from './App.tsx'
@@ -7,8 +8,11 @@ import App from './App.tsx'
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
     <Routes>
-       <Route path="/" element={<App />} />
-       <Route path="/lobby" element={<Lobby />} />
+      <Route path="/" element={<App />} />
+
+      <Route element={<ProtectedRoute />}>
+        <Route path="/lobby" element={<Lobby />} />
+      </Route>
     </Routes>
   </BrowserRouter>
 )
--- frontend/frontend/src/pages/ProtectedRoute.tsx
+++ frontend/frontend/src/pages/ProtectedRoute.tsx
@@ -1,3 +1,12 @@
+import { isAuthenticated } from "../services/nakama"
+import { Navigate, Outlet } from "react-router";
+
 const ProtectedRoute = () => {
-    
+    if (!isAuthenticated()) {
+        return <Navigate to="/" replace />;
+    }
+
+    return <Outlet />;
 }
+
+export default ProtectedRoute;
