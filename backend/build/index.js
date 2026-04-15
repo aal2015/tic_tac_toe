3f656254dd4ce6a6c090481fe06f98bcce0f434a
aal2015 <abhinavlugun@yahoo.com>
2026-04-15 14:26:47 +0530

    Added healthcheck function to test the registration of custom logic

--- backend/build/index.js
+++ backend/build/index.js
@@ -1,4 +1,9 @@
 "use strict";
 function InitModule(ctx, logger, nk, initializer) {
+    initializer.registerRpc('healthcheck', rpcHealthCheck);
     logger.info('JavaScript logic loaded.');
+}
+function rpcHealthCheck(ctx, logger, nk, payload) {
+    logger.info('healtcheck rpc called');
+    return JSON.stringify({ success: true });
 }
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,4 +1,5 @@
 function InitModule(ctx: nkruntime.Context, logger: nkruntime.Logger, nk: nkruntime.Nakama, initializer: nkruntime.Initializer) {
+    initializer.registerRpc('healthcheck', rpcHealthCheck);
     logger.info('JavaScript logic loaded.');
 }
 
